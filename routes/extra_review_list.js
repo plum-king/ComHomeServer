719ed48b70706f66ab8de4d7747bf57d15b03d56
JIHYUN LEE <20201008@sungshin.ac.kr>
2022-07-14 01:28:40 +0900

    [FIX] 글 작성시간 표시

--- routes/extra_review_list.js
+++ routes/extra_review_list.js
@@ -12,16 +12,13 @@ router.get("/extra_review_list", async (req, res) => {
   const data = await pool.query(`SELECT * FROM extra_review ORDER BY review_no DESC`);
   const time_data = await pool.query(`SELECT date_format(upload_time, '%Y-%m-%d') FROM extra_review`);
   let data_det = data[0];
-  let data_time = time_data[0];
-  console.log(data_time);
-  //   console.log(data_det.length);
 
   while (i < data_det.length) {
     const data2 = await pool.query(`SELECT name FROM user where iduser = ?`, [
       data_det[i].iduser,
     ]);
     // console.log(data2[0][0].name);
-    body += `<a href = "/extra_review_detail/${data_det[i].review_no}">${data_det[i].review_no}</a> | ${data_det[i].review_title} | ${data_time[0]}} <br>`;
+    body += `<a href = "/extra_review_detail/${data_det[i].review_no}">${data_det[i].review_no}</a> | ${data_det[i].review_title} | ${time_data[0][i]["date_format(upload_time, '%Y-%m-%d')"]} <br>`;
     i++;
   }
   body += `<a href = "/extra_review_write">대외활동 후기글 작성하러 가기</a>`;
