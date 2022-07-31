eff28a552ae0d59cec23cde55679466171e2fe84
leejw-lu <jiwoojenny@naver.com>
2022-08-01 02:45:53 +0900

    [add] success to connect with FE

--- routes/recruit_internship_detail.js
+++ routes/recruit_internship_detail.js
@@ -16,45 +16,15 @@ router.get("/:notice_id", async (req, res) => {
         console.error(err);
     }
 
-    const title = notice_id + "번 게시글";
-    const head = `<meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">`;
     const data = await pool.query(`SELECT * FROM recruit_intern where notice_id = ?`,[notice_id]);
     const file_data = await pool.query(`SELECT * FROM file_intern where notice_id = ?`,[notice_id]);
-    const time_data = await pool.query(`SELECT date_format(not_created_date, '%Y-%m-%d') FROM recruit_intern`);
-    const time_data2 = await pool.query(`SELECT date_format(not_edited_date, '%Y-%m-%d') FROM recruit_intern`);
+    //const time_data = await pool.query(`SELECT date_format(not_created_date, '%Y-%m-%d') FROM recruit_intern`);
+    //const time_data2 = await pool.query(`SELECT date_format(not_edited_date, '%Y-%m-%d') FROM recruit_intern`);
 
-    //console.log(data[0][0].not_img);
-
-    let body = `<p>제목: ${data[0][0].not_title}</p>
-    <p>작성일: ${time_data[0][0]["date_format(not_created_date, '%Y-%m-%d')"]}</p>
-    <p>수정일: ${time_data2[0][0]["date_format(not_edited_date, '%Y-%m-%d')"]}</p>
-    <p>조회수: ${data[0][0].not_views}</p>
-    <p>글번호: ${notice_id}</p>
-    <p><b>=첨부파일=</b></p>
-    `
-    console.log(file_data[0]);
-    if(file_data[0].length>0){
-     for(let i=0; i < file_data.length; i++) {
-        //console.log(file_data[0][i]);
-        //console.log(file_data[0][i].file_infoN);
-        let filename=file_data[0][i].file_infoN.substr(8);
-        body +=`<a href = "/api/download/${filename}">${file_data[0][i].file_originN}</a><br>`;
-        }
-    }else{
-        body +=`<p>첨부파일이(가) 없습니다.</p>`
-    }
-    body+=`
-    <hr>
-    <div>
-    <img src="${data[0][0].not_img}" />
-    </div>
-    <p>내용: ${data[0][0].not_content}</p>
-    <a href = "/api/recruit_internship_list">목록으로 돌아가기</a>
-    `;
-    var html = templates.HTML(title, head, body);
-    res.send(html);
+    res.json({
+        data_det:data[0][0],
+        file:file_data[0]
+    });
 });
 
 module.exports = router;
--- routes/recruit_internship_list.js
+++ routes/recruit_internship_list.js
@@ -7,26 +7,12 @@ const path = require('path');
 
 //채용/인턴십 목록보기
 router.get("/", async (req, res) => {
-    const title = "공지사항-채용인턴십";
-    const head = ``;
-    let body = `게시글 넘버 | 게시글 제목 | 조회수 | 작성 날짜 <br>`;
     let i = 0;
     const data = await pool.query(`SELECT * FROM recruit_intern ORDER BY not_created_date DESC`);
     const time_data = await pool.query(`SELECT date_format(not_created_date, '%Y-%m-%d') FROM recruit_intern ORDER BY not_created_date DESC`);
     let data_det = data[0];
-    //console.log(data_det);
-  
-    while (i < data_det.length) {
-      const data2 = await pool.query(`SELECT name FROM user where iduser = ?`, [
-        data_det[i].user_id,
-      ]);
-      // console.log(data2[0][0].name);
-      body += `<a href = "/api/recruit_internship_detail/${data_det[i].notice_id}">${data_det.length-i}</a> | ${data_det[i].not_title} | ${data_det[i].not_views} | ${time_data[0][i]["date_format(not_created_date, '%Y-%m-%d')"]} <br>`;
-      i++;
-    }
-    body += `<br><a href = "/api/recruit_internship/post">채용인턴십 글 작성하기</a>`;
-    var html = templates.HTML(title, head, body);
-    res.send(html);
+
+    res.json({data_det: data_det});
   });
 
   module.exports = router;
