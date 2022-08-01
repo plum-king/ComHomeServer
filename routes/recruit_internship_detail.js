f4bb810f12b9287e30cd843e674598454a96cd29
leejw-lu <jiwoojenny@naver.com>
2022-08-02 01:40:15 +0900

    [add] 한글파일 원본명대로 다운 가능

--- routes/recruit_internship_detail.js
+++ routes/recruit_internship_detail.js
@@ -11,20 +11,52 @@ router.get("/:notice_id", async (req, res) => {
 
     //조회수 +1
     try {
-        const data = await pool.query("UPDATE recruit_intern set not_views=not_views+1 where notice_id =? ",[notice_id]);
+        const data = await pool.query("UPDATE recruit_intern set not_views=not_views+1 where no =? ",[notice_id]);
     } catch (err) {
         console.error(err);
     }
 
-    const data = await pool.query(`SELECT * FROM recruit_intern where notice_id = ?`,[notice_id]);
-    const file_data = await pool.query(`SELECT * FROM file_intern where notice_id = ?`,[notice_id]);
-    //const time_data = await pool.query(`SELECT date_format(not_created_date, '%Y-%m-%d') FROM recruit_intern`);
-    //const time_data2 = await pool.query(`SELECT date_format(not_edited_date, '%Y-%m-%d') FROM recruit_intern`);
+    const title = notice_id + "번 게시글";
+    const head = `<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">`;
 
-    res.json({
-        data_det:data[0][0],
-        file:file_data[0]
-    });
+    const data = await pool.query(`SELECT * FROM recruit_intern where no = ?`,[notice_id]);
+    const file_data = await pool.query(`SELECT * FROM file_intern where notice_id = ?`,[notice_id]);
+    const time_data = await pool.query(`SELECT date_format(not_created_date, '%Y-%m-%d') FROM recruit_intern`);
+    const time_data2 = await pool.query(`SELECT date_format(not_edited_date, '%Y-%m-%d') FROM recruit_intern`);
+    //console.log(data[0][0].not_img);
+
+    let body = `<p>제목: ${data[0][0].not_title}</p>
+    <p>작성일: ${time_data[0][0]["date_format(not_created_date, '%Y-%m-%d')"]}</p>
+    <p>수정일: ${time_data2[0][0]["date_format(not_edited_date, '%Y-%m-%d')"]}</p>
+    <p>조회수: ${data[0][0].not_views}</p>
+    <p>글번호: ${notice_id}</p>
+    <p><b>=첨부파일=</b></p>
+    `
+    console.log(file_data[0]);
+    if(file_data[0].length>0){
+     for(let i=0; i < file_data[0].length; i++) {   //file_data[0].length [0]을 하자!!
+        let filename=file_data[0][i].file_infoN.substr(8);
+
+        const file = 'uploads/' + filename;
+
+        body +=`<a href = "/api/download/${filename}">${file_data[0][i].file_originN}</a><br>`;
+        }
+    }else{
+        body +=`<p>첨부파일이(가) 없습니다.</p>`
+    }
+
+    body+=`
+    <hr>
+    <div>
+    <img src="${data[0][0].not_img}" />
+    </div>
+    <p>내용: ${data[0][0].not_content}</p>
+    <a href = "/api/recruit_internship_list">목록으로 돌아가기</a>
+    `;
+    var html = templates.HTML(title, head, body);
+    res.send(html);
 });
 
 module.exports = router;
