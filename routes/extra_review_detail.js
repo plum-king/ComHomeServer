09deb906ef434abe4e4e1390cb390a4bc6e84fa7
Gongdongjang <jeesoo0908@gmail.com>
2022-08-03 18:41:48 +0900

    [mod] extra_review 속성명

--- routes/extra_review_detail.js
+++ routes/extra_review_detail.js
@@ -6,16 +6,15 @@ const templates = require("../lib/templates");
 const path = require("path");
 
 router.get("/:review_no", async (req, res) => {
-  if(!req.user.id) res.send("로그인이 필요한 서비스 입니다.");
+  if (!req.user.id) res.send("로그인이 필요한 서비스 입니다.");
 
   const review_no = path.parse(req.params.review_no).base;
   console.log(review_no);
   const title = review_no + "번 게시글";
   const head = ``;
-  const data = await pool.query(
-    `SELECT * FROM extra_review where review_no = ?`,
-    [review_no]
-  );
+  const data = await pool.query(`SELECT * FROM extra_review where no = ?`, [
+    review_no,
+  ]);
   const body = `<p>${data[0][0].review_title}</p> 
   <p>${data[0][0].review_cont}</p>
   <a href = "/api/extra_review_list">목록으로 돌아가기</a>
