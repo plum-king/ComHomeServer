3a0c9dc9efe5224fe5d87ac5a33420411e18fc77
Gongdongjang <jeesoo0908@gmail.com>
2022-08-14 17:12:10 +0900

    [add] callback

--- routes/auth.js
+++ routes/auth.js
@@ -1,30 +1,68 @@
-const express = require('express');
-const router   = express.Router();
-const passport = require('../config/passport.js');
+// const express = require('express');
+// const router   = express.Router();
+// const passport = require('../config/passport.js');
 
-router.get('/login', async (req, res) => {
-  res.redirect('/api/auth/google');
+// router.get('/login', async (req, res) => {
+//   res.redirect('/api/auth/google');
+// });
+
+// router.get('/google', passport.authenticate('google', {
+//   scope: ['email','profile']
+// }
+// ));
+
+// router.get('/google/callback',
+//   passport.authenticate('google'), authSuccess
+// );
+
+// function authSuccess(req, res) {
+//   res.redirect('/');
+// }
+
+// router.get('/logout', function(req, res, next) {
+//   req.logout(function(err) {
+//     if (err) { return next(err); }
+//     res.redirect('/');
+//   });
+// });
+
+// module.exports = router;
+
+const express = require("express");
+const router = express.Router();
+const passport = require("../config/passport.js");
+
+router.get("/login", async (req, res) => {
+  res.redirect("/api/auth/google");
 });
 
-router.get('/google', passport.authenticate('google', { 
-  scope: ['email','profile']
-} 
-));
-
-router.get('/google/callback',
-  passport.authenticate('google'), authSuccess
+router.get(
+  "/google",
+  passport.authenticate("google", {
+    scope: ["email", "profile"],
+  })
 );
 
+router.get("/google/callback", passport.authenticate("google"), authSuccess);
+
 function authSuccess(req, res) {
-  res.redirect('/');
+  console.log("req.user : " + JSON.stringify(req.user));
+  res.redirect(`http://localhost:3000/userid/${req.user.id}`);
 }
 
-router.get('/logout', function(req, res, next) {
-  req.logout(function(err) {
-    if (err) { return next(err); }
-    res.redirect('/');
+router.post("/userinfo", function (req, res, next) {
+  res.json({
+    userinfo: req.user,
   });
 });
 
+router.get("/logout", function (req, res, next) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
 
 module.exports = router;
