f93dade37158614cd093bffb67b9ae576aaa53cb
leejw-lu <jiwoojenny@naver.com>
2022-08-16 23:56:31 +0900

    [fix] socket.rooms type set to array

--- routes/chat_practice_bubble.js
+++ routes/chat_practice_bubble.js
@@ -10,10 +10,10 @@ router.get("/", async (req,res)=>{
     let io = req.app.get('socketio');
 
     io.on('connection', (socket)=>{
-        socket.on('request_message', (msg) => {
-            // response_message로 접속중인 모든 사용자에게 msg 를 담은 정보를 방출한다.
-            io.emit('response_message', msg);
-        });
+        // socket.on('request_message', (msg) => {
+        //     // response_message로 접속중인 모든 사용자에게 msg 를 담은 정보를 방출한다.
+        //     io.emit('response_message', msg);
+        // });
 
         // 방참여 요청
         socket.on('req_join_room', async (msg) => {
@@ -23,37 +23,32 @@ router.get("/", async (req,res)=>{
             }else{
                 
             }
-            console.log("roomName:"+ roomName);
+            //console.log("roomName:"+ roomName);
             socket.join(roomName);
             io.to(roomName).emit('noti_join_room', "방에 입장하였습니다.");
         });
 
         // 채팅방에 채팅 요청
         socket.on('req_room_message', async (msg) => {
-            console.log(socket.rooms);
-            //console.log(io.sockets.adapter.rooms);
-            //let userCurrentRoom = getUserCurrentRoom(socket);
+            //console.log(socket.rooms);
 
-            //console.log("getUserCurentRoom 함수 왔니..?");
             let currentRoom = '';
-            let socketRooms = Object.keys(socket.rooms);
-            console.log("socket.rooms:");
-            console.log(socket.rooms);
-            console.log(socketRooms);
+            let socketRooms = socket.rooms;
+            console.log(socket.rooms.size); //socket.rooms은 set 속성 >> size
+
+            let arr = Array.from(socketRooms);
         
-            for( let i = 0 ; i < socket.rooms.length; i ++ ){
-                if(socket.rooms[i].indexOf('Room_') !== -1){
-                    currentRoom = socket.rooms[i];
+            for( let i = 0 ; i < socketRooms; i ++ ){
+                if(arr[i].indexOf('Room_') !== -1){
+                    currentRoom = arr[i];
                     console.log(currentRoom);
                     break;
                 } 
             }
-            //console.log(currentRoom);
-            //return currentRoom;
-
-            //console.log("userCurrentRoom:" + CurrentRoom);
             console.log("메시지:"+ msg);
-            io.to('Room_test-room').emit('noti_room_message', msg);
+            //io.to('Room_아이디').emit('noti_room_message', msg);
+            io.to(currentRoom).emit('noti_room_message', msg);
+
         });
 
         socket.on('disconnect', async () => {
@@ -83,4 +78,5 @@ router.get("/", async (req,res)=>{
 //     console.log(currentRoom);
 //     return currentRoom;
 // }
+
 module.exports = router;
